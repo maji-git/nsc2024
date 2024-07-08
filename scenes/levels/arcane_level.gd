bff4afb8f13859921988ffe69659276222985419
Phatphum221 <phumfirebase@gmail.com>
2024-07-08 14:58:36 +0700

    arcane levels added (wip)

---
+++ scenes/levels/arcane_level.gd
@@ -0,0 +1,13 @@
+class_name ArcaneLevel
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#var arcane_room1 := ArcaneRoom.new(false)
+	#add_child(arcane_room1)
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
